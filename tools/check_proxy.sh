9380327728c57e314f16ae304f5034e155259629
Phu Thai HO <phu.ho@nokia.com>
2019-02-14 09:33:57 +0100

    Check proxy networks

---
+++ tools/check_proxy.sh
@@ -0,0 +1,14 @@
+PROXY_FILE="/etc/apt/apt.conf.d/95proxies";
+ENV_FILE="/etc/environment";
+
+check_proxy() {
+	local line=$(head -n 2 "${1}" | tail -n 1);
+	if [[ "${line}" == "#"* ]]; then
+		echo "External: ${1}";
+	else
+		echo "Internal ${1}";
+	fi
+}
+
+check_proxy "${PROXY_FILE}";
+check_proxy "${ENV_FILE}";
