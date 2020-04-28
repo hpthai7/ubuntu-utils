2e42cc3b79249d1bdc38c7015736cc3105364f8f
Phu Thai HO <hophuthai@gmail.com>
2020-04-28 19:46:16 +0200

    Add sourcing zshrc

--- install/install-git.sh
+++ install/install-git.sh
@@ -9,12 +9,16 @@ cp gitconfig ~/.gitconfig;
 # add aliases
 cp git-aliases ~/.gitrc
 echo "source ~/.gitrc" >> ~/.bashrc
+echo "source ~/.gitrc" >> ~/.zshrc
 source ~/.bashrc
+source ~/.zshrc
 
 # add k8s aliases
 cp k8s-aliases ~/.kuberc
 echo "source ~/.kuberc" >> ~/.bashrc
+echo "source ~/.kuberc" >> ~/.zshrc
 source ~/.bashrc
+source ~/.zshrc
 
 # generate ssh keys
 ssh-keygen -t rsa;
