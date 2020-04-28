a6ee6be38719212e78a5876a5e7547594f467543
Phu Thai HO <hophuthai@gmail.com>
2020-04-28 15:14:46 +0000

    Unhide gitconfig

--- install/install-git.sh
+++ install/install-git.sh
@@ -4,11 +4,16 @@ sudo apt-get update
 sudo apt install -y git || exit 1;
 
 # add config files
-cp .git* ~;
+cp gitconfig ~/.gitconfig;
 
 # add aliases
 cp git-aliases ~/.gitrc
 echo "source ~/.gitrc" >> ~/.bashrc
+source ~/.bashrc
+
+# add k8s aliases
+cp k8s-aliases ~/.kuberc
+echo "source ~/.kuberc" >> ~/.bashrc
 source ~/.bashrc
 
 # generate ssh keys
