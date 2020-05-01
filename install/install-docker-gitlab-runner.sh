18d093872089a6eae3031e7b7289e640370dbbb2
Phu Thai HO <hophuthai@gmail.com>
2020-05-01 11:29:27 +0000

    Add comments on gitlab runner mounting config

--- install/install-docker-gitlab-runner.sh
+++ install/install-docker-gitlab-runner.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 docker run -d --name gitlab-runner-config \
-    -v /etc/gitlab-runner \
+    -v /etc/gitlab-runner \ # mount host's /var/lib/docker volume to container's /etc/gitlab-runner
     busybox:latest \
     /bin/true
 
 docker run -d --name gitlab-runner --restart always \
-    -v /var/run/docker.sock:/var/run/docker.sock \
-    --volumes-from gitlab-runner-config \
+    -v /var/run/docker.sock:/var/run/docker.sock \ # mount docker socket of host to gitlab-runner container
+    --volumes-from gitlab-runner-config \ # use the entire mount config of the gitlab-runner-config container
     gitlab/gitlab-runner:latest
