bf5a2e5c25dc14e8d336ed6a3936d5e2160a8b3c
Ho Phu Thai <phu.ho@nokia-bell-labs.com>
2020-05-17 02:57:34 +0200

    Add command-not-found package

--- tools/auto-detect-command.sh
+++ tools/auto-detect-command.sh
@@ -1,1 +1,3 @@
-sudo pt-get install -y command-not-found
+# providing suggestions for non-installed packages
+# https://unix.stackexchange.com/questions/67466/how-to-implement-package-install-suggestion-on-debian
+sudo apt-get install -y command-not-found
